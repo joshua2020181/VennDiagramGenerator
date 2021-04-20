600329d5294f55226aba3c16feec4783d5f85ba6
Joshua Cheng <josh.cheng.sh@gmail.com>
2021-04-20 06:27:04 -0400

    settings allowed host

--- vennGenerator/settings.py
+++ vennGenerator/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-#5#*m094@=9u1l11b@zr4lpopjmq%mwx&$s4^c4z)l2x6uvdvs
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['http://chengjo.pythonanywhere.com/']
 
 
 # Application definition
